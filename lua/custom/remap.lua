32e91e8eca64a4af8e2122b7f6ff6217af03e5aa
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2024-02-23 22:34:33 -0300

    add keymaps to save, quit and fugitive

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -1,11 +1,11 @@
 local function nmap(key, command, opts)
-  opts = opts or {}
-	vim.keymap.set('n', key, command,opts)
+	opts = opts or {}
+	vim.keymap.set('n', key, command, opts)
 end
 
 local function vmap(key, command, opts)
-  opts = opts or {}
-	vim.keymap.set('v', key, command,opts)
+	opts = opts or {}
+	vim.keymap.set('v', key, command, opts)
 end
 
 vmap("J", ":m '>+1<CR>gv=gv")
@@ -15,6 +15,12 @@ nmap("<C-d>", "<C-d>zz")
 nmap("<C-u>", "<C-u>zz")
 nmap("n", "nzzzv")
 nmap("N", "Nzzzv")
+nmap('<leader>ww', '<cmd>w<cr>', { desc = "Write the changes made in current buffer" })
+nmap('<leader>qq', '<cmd>q<cr>', { desc = "Quit" })
 
 -- OIL
 nmap("-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Fugitive
+nmap('<leader>gc', '<cmd>Git commit<cr>', { desc = '[G]it [C]ommit' })
+nmap('<leader>gp', '<cmd>Git push<cr>', { desc = '[G]it [P]ush' })
